0be393452e330c01d106ec63c3b59a1bce21d466
Jay Rathod <jay.rathod@simformsolutions.com>
2023-04-17 02:26:14 +0530

    update: updated exprience

--- script.js
+++ script.js
@@ -75,18 +75,30 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Developer",],
-        typeSpeed: 100,
-        backSpeed: 60,
+        strings: ["Back-end developer working in Spring boot",],
+        typeSpeed: 30,
+        backSpeed: 20,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
+      strings: ["Skilled backend developer with 2 years of experience in developing scalable and efficient applications using Spring Boot framework. At Simform Solutions, I have designed and implemented RESTful APIs, integrated with various third-party services, and maintained robust codebases to support mission-critical business operations. I am passionate about delivering high-quality software solutions that meet business requirements and strive to continuously improve my skills and knowledge in the ever-evolving technology landscape."],
+      typeSpeed: 10,
+      backSpeed: 5,
+      loop: false,
+
+
+     
+      preStringTyped: function() {
+        var element = document.querySelector(".typing-2");
+        element.style.background = "-webkit-linear-gradient(right, crimson, white)";
+        element.style.webkitBackgroundClip = "text";
+        element.style.webkitTextFillColor = "transparent";
+      }
+      
+      
     });
+    
 
     // owl carousel script
     $('.carousel').owlCarousel({
